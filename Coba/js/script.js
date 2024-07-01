b9aa8f43088f885af186f784735d9014a221735f
Randif <randifebriadi@gmail.com>
2024-07-01 10:11:52 +0700

    Delete Coba directory

--- Coba/js/script.js
+++
@@ -1,6 +0,0 @@
-const menuToggle = document.querySelector(".menu-toggle");
-const nav = document.querySelector("nav ul");
-
-menuToggle.addEventListener("click", function () {
-  nav.classList.toggle("slide");
-});
